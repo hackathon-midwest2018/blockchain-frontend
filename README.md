c45695d94ed8a38f1f955699a8929c127d9b268a
Patrick Burns <burnsoft@gmail.com>
2018-07-21 15:55:27 -0500

    starting over again

---
+++ README.md
@@ -0,0 +1,20 @@
+# AngularThreeExamples
+[Angular5](https://angular.io) + [three.js](https://github.com/mrdoob/three.js) + Three.js examples/js (ColladaLoader and OrbitControls)  
+
+## Demo
+See [DEMO](https://makimenko.github.io/angular-three-examples)
+
+## Installation
+```
+git clone https://github.com/makimenko/angular-three-examples.git
+cd angular-three-examples
+npm install
+ng serve
+```
+
+## Angular 6
+Development switched to [angular-template-for-dummies](https://github.com/makimenko/angular-template-for-dummies/tree/threejs) repository.
+ 
+## Angular 4
+Previous version of Angular4 see in tag: [ng4](https://github.com/makimenko/angular-three-examples/tree/ng4)
+ 
---
+++ src/app/scene/scene.component.ts
@@ -0,0 +1,206 @@
+import { AfterViewInit, Component, ElementRef, Input, ViewChild, HostListener } from '@angular/core';
+import * as THREE from 'three';
+import "./js/EnableThreeExamples";
+import "three/examples/js/controls/OrbitControls";
+import "three/examples/js/loaders/ColladaLoader";
+import "three/examples/js/loaders/ColladaLoader";
+
+@Component({
+    selector: 'scene',
+    templateUrl: './scene.component.html',
+    styleUrls: ['./scene.component.css']
+})
+export class SceneComponent implements AfterViewInit {
+
+    private renderer: THREE.WebGLRenderer;
+    private camera: THREE.PerspectiveCamera;
+    private cameraTarget: THREE.Vector3;
+    private clock: THREE.Clock;
+    private mixer: THREE.AnimationMixer;
+    public scene: THREE.Scene;
+
+    public fieldOfView: number = 60;
+    public nearClippingPane: number = 1;
+    public farClippingPane: number = 1100;
+
+    public controls: THREE.OrbitControls;
+
+    @ViewChild('canvas')
+    private canvasRef: ElementRef;
+
+    constructor() {
+        this.render = this.render.bind(this);
+        this.onModelLoadingCompleted = this.onModelLoadingCompleted.bind(this);
+    }
+
+    private get canvas(): HTMLCanvasElement {
+        return this.canvasRef.nativeElement;
+    }
+
+    private createScene() {
+        this.scene = new THREE.Scene();
+        // this.scene.add(new THREE.AxisHelper(200));
+        var loader = new THREE.ObjectLoader();
+        loader.load('assets/model/psc-warrior.json', this.onModelLoadingCompleted);
+    }
+
+    private onModelLoadingCompleted(collada) {
+        // var modelScene = collada.scene;
+        console.log('collada: ', collada)
+        // console.log('model: ', modelScene)
+        this.scene.add(collada);
+        var sceneAnimationClip = collada.animations[0];
+
+         // Create animation mixer and pass object to it
+         this.mixer = new THREE.AnimationMixer(collada);
+
+         // Create animation action and start it
+         var sceneAnimation = this.mixer.clipAction(sceneAnimationClip);
+         sceneAnimation.play();
+        this.render();
+    }
+
+    private createLight() {
+        var light = new THREE.PointLight(0xffffff, 1, 1000);
+        light.position.set(0, 0, 100);
+        this.scene.add(light);
+
+        var light = new THREE.PointLight(0xffffff, 1, 1000);
+        light.position.set(0, 0, -100);
+        this.scene.add(light);
+    }
+
+    private createGrid() {
+      var size = 8;
+      var divisions = 8;
+
+      var gridHelper = new THREE.GridHelper( size, divisions );
+      this.scene.add( gridHelper )
+    }
+
+    private createCamera() {
+        let aspectRatio = this.getAspectRatio();
+        this.camera = new THREE.PerspectiveCamera(
+            this.fieldOfView,
+            aspectRatio,
+            this.nearClippingPane,
+            this.farClippingPane
+        );
+
+        // Set position and look at
+        this.camera.position.x = 10;
+        this.camera.position.y = 10;
+        this.camera.position.z = 5;
+    }
+
+    private getAspectRatio(): number {
+        let height = this.canvas.clientHeight;
+        if (height === 0) {
+            return 0;
+        }
+        return this.canvas.clientWidth / this.canvas.clientHeight;
+    }
+
+    private startRendering() {
+        this.renderer = new THREE.WebGLRenderer({
+            canvas: this.canvas,
+            antialias: true
+        });
+        this.renderer.setPixelRatio(devicePixelRatio);
+        this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
+
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+        this.renderer.setClearColor(0x222222, 1);
+        this.renderer.autoClear = true;
+
+        let component: SceneComponent = this;
+        this.clock = new THREE.Clock();
+
+        (function render() {
+            requestAnimationFrame(render);
+            component.render();
+        }.bind(this));
+    }
+
+    public render() {
+        let delta = this.clock.getDelta();
+        this.mixer.update( delta );
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    public addControls() {
+        this.controls = new THREE.OrbitControls(this.camera);
+        this.controls.rotateSpeed = 1.0;
+        this.controls.zoomSpeed = 1.2;
+        this.controls.addEventListener('change', this.render);
+
+    }
+
+    /* EVENTS */
+
+    public onMouseDown(event: MouseEvent) {
+        console.log("onMouseDown");
+        event.preventDefault();
+
+        // Example of mesh selection/pick:
+        var raycaster = new THREE.Raycaster();
+        var mouse = new THREE.Vector2();
+        mouse.x = (event.clientX / this.renderer.domElement.clientWidth) * 2 - 1;
+        mouse.y = - (event.clientY / this.renderer.domElement.clientHeight) * 2 + 1;
+        raycaster.setFromCamera(mouse, this.camera);
+
+        var obj: THREE.Object3D[] = [];
+        this.findAllObjects(obj, this.scene);
+        var intersects = raycaster.intersectObjects(obj);
+        console.log("Scene has " + obj.length + " objects");
+        console.log(intersects.length + " intersected objects found")
+        intersects.forEach((i) => {
+            console.log(i.object); // do what you want to do with object
+        });
+
+    }
+
+    private findAllObjects(pred: THREE.Object3D[], parent: THREE.Object3D) {
+        // NOTE: Better to keep separate array of selected objects
+        if (parent.children.length > 0) {
+            parent.children.forEach((i) => {
+                pred.push(i);
+                this.findAllObjects(pred, i);
+            });
+        }
+    }
+
+    public onMouseUp(event: MouseEvent) {
+        console.log("onMouseUp");
+    }
+
+
+    @HostListener('window:resize', ['$event'])
+    public onResize(event: Event) {
+        this.canvas.style.width = "100%";
+        this.canvas.style.height = "100%";
+        console.log("onResize: " + this.canvas.clientWidth + ", " + this.canvas.clientHeight);
+
+        this.camera.aspect = this.getAspectRatio();
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
+        this.render();
+    }
+
+    @HostListener('document:keypress', ['$event'])
+    public onKeyPress(event: KeyboardEvent) {
+        console.log("onKeyPress: " + event.key);
+    }
+
+    /* LIFECYCLE */
+    ngAfterViewInit() {
+        this.createScene();
+        this.createLight();
+        this.createCamera();
+        this.createGrid();
+        this.startRendering();
+        this.addControls();
+    }
+
+}
